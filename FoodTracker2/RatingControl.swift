d6a8f024fa884d8d60c699c243ef848ac6be82e9
sarahflestari <sarahfauziah17@gmail.com>
2018-06-06 16:39:59 +0700

    Rating button UI

--- FoodTracker2/RatingControl.swift
+++ FoodTracker2/RatingControl.swift
@@ -9,13 +9,46 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    
+    // MARK : Properties
+    private var ratingButtons = [UIButton]()
+    var rating = 0
+    
     // MARK : Initialization
     
-    init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: frame)
+        setupButtons()
     }
-    init(coder: NSCoder) {
+    required init(coder: NSCoder) {
         super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK : Button Action
+    @objc func ratingButtonTapped(button: UIButton){
+        print("Button Pressed")
+    }
+    
+    // MARK : Private Methods
+    
+    private func setupButtons(){
+        for _ in 0..<5 {
+            // Create the button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+        
+            // Add Constraint
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+            // Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+        
+            // Add the button to the stack
+            addArrangedSubview(button)
+        }
     }
 
 }
