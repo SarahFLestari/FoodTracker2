9fb6ebe229239a1e1fc6062131df4692c1ba78b5
sarahflestari <sarahfauziah17@gmail.com>
2018-06-07 10:25:17 +0700

    Star View

--- FoodTracker2/RatingControl.swift
+++ FoodTracker2/RatingControl.swift
@@ -8,9 +8,19 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
     
     // MARK : Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet{
+            setupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5{
+        didSet{
+            setupButtons()
+        }
+    }
     private var ratingButtons = [UIButton]()
     var rating = 0
     
@@ -33,15 +43,33 @@ class RatingControl: UIStackView {
     // MARK : Private Methods
     
     private func setupButtons(){
-        for _ in 0..<5 {
+        // Clear any existing buttons
+        for button in ratingButtons{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        // Load button image
+        let bundle = Bundle(for: type(of:self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        for _ in 0..<starCount {
             // Create the button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            
+            // Set the button images
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
         
             // Add Constraint
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
         
             // Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
